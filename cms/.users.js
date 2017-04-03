07866662a945804867064d1740bfa6955e706b61
Richard van der Dys <richard.vanderdys@gmail.com>
2017-04-03 21:26:35 +0300

    Initial commit

---
+++ cms/.users.js
@@ -0,0 +1,11 @@
+{
+	users: [
+		{
+			username: 'admin',
+			tags: [],
+			salt: '8176502ff980ead99ae1ece69fb3072e',
+			hash: '65e7e8cf8951aec6d8c35822760f43833c13abf193e265d94fed8fd350ee1a53',
+			user_created_timestamp: 1491218160414
+		}
+	]
+}
---
+++ cms/index.js
@@ -0,0 +1,14 @@
+{
+	title: 'Csakis Festival!',
+	greeting: 'Welcome!',
+	subtitle: 'We love Jesus!',
+	section_one_title: 'Családos vasárnap havonta',
+	section_one_body_text: 'Másfél évvel ezelött indítottuk a Családos Vasárnapot az Oroszfalvi templomban.\nMinden hónap végèn kerül sor ez alkalomra, amely mindenkinek szòl, felekezetre való tekintet nèlkül jönnek a házaspàrok, illetve családok a 12 órától kezdödö eseményre.Mi is történik ilyenkor?\nDicsöítès, köszöntés, ima, majd a tanítás következik. (ezt házaspárok tartják, lehetöség van a szolgálatra, aki erre elhívàst érez)Elcsendesedés után ismèt énekelünk és szeretetvendégséggel zárjuk az összejövetelt.\nA gyermekekkel vasárnapi iskolás oktatók foglalkoznak.Szeretettel várunk alkalmainkra minden házaspárt, csalàdot, aki ùgy érzi, van még mit tanulnia Istentöl, testvéreinken keresztül.',
+	$section_one_body_text_type: 'textarea',
+	section_one_image: '/remote/direct_uploads/1491223683_Csakis-3-48.jpg',
+	$section_one_image_type: 'image',
+	$banner_image_type: 'image',
+	banner_image: '/remote/direct_uploads/1491223979_CSAKISbannerlong.jpg.jpeg',
+	$logo_type: 'image',
+	logo: '/remote/direct_uploads/1491223961_CSAKISbannerlong.jpg.jpeg'
+}
