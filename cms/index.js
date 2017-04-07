ecdf7d7e302249580d6a450ae9221a2b19c93c25
Richard van der Dys <richard.vanderdys@gmail.com>
2017-04-07 12:01:37 +0300

    Made default remote images

--- cms/index.js
+++ cms/index.js
@@ -5,10 +5,10 @@
 	section_one_title: 'Családos vasárnap havonta',
 	section_one_body_text: 'Másfél évvel ezelött indítottuk a Családos Vasárnapot az Oroszfalvi templomban.\nMinden hónap végèn kerül sor ez alkalomra, amely mindenkinek szòl, felekezetre való tekintet nèlkül jönnek a házaspàrok, illetve családok a 12 órától kezdödö eseményre.Mi is történik ilyenkor?\nDicsöítès, köszöntés, ima, majd a tanítás következik. (ezt házaspárok tartják, lehetöség van a szolgálatra, aki erre elhívàst érez)Elcsendesedés után ismèt énekelünk és szeretetvendégséggel zárjuk az összejövetelt.\nA gyermekekkel vasárnapi iskolás oktatók foglalkoznak.Szeretettel várunk alkalmainkra minden házaspárt, csalàdot, aki ùgy érzi, van még mit tanulnia Istentöl, testvéreinken keresztül.',
 	$section_one_body_text_type: 'textarea',
-	section_one_image: '/remote/direct_uploads/1491223683_Csakis-3-48.jpg',
+	section_one_image: 'http://res.cloudinary.com/csakis-fesztival/image/upload/v1491554035/sample.jpg',
 	$section_one_image_type: 'image',
 	$banner_image_type: 'image',
-	banner_image: '/remote/direct_uploads/1491223979_CSAKISbannerlong.jpg.jpeg',
+	banner_image: 'http://res.cloudinary.com/csakis-fesztival/image/upload/c_scale,w_1371/v1491554140/CSAKISbannerlong-withdates_u1k9ml.jpg',
 	$logo_type: 'image',
-	logo: '/remote/direct_uploads/1491223961_CSAKISbannerlong.jpg.jpeg'
+	logo: 'http://res.cloudinary.com/csakis-fesztival/image/upload/v1491554139/CSAKISsquareTRANSPARENTjustlogo_f90dn3.png'
 }
